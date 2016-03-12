c5f43a1770e476af30ecdd31859657339d0f7654
meun5 <clamy4@msn.com>
2016-03-12 13:03:04 -0600

    Added maintaince check

--- public/index.php
+++ public/index.php
@@ -10,6 +10,13 @@
  * @version     v1-beta
  */
 
+if (file_exists("../.maintaince")) {
+	header("Cache-Control: no-cache, no-store, must-revalidate post-check=0, pre-check=0");
+	header("Pragma: no-cache");
+	echo file_get_contents("../asap-scouting/views/templates/maintaince.twig");
+	die();
+}
+
 require_once("../asap-scouting/start.php");
 
 $app->run();
